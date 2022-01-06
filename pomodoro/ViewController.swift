3a20d8e4eea855ddd9acbb557bac8746d9071617
joensumin <soomis7@naver.com>
2022-01-06 15:04:14 +0900

    타이머의 상태에 따라 화면구성이 달라 지게 구현

--- pomodoro/ViewController.swift
+++ pomodoro/ViewController.swift
@@ -7,13 +7,77 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum TimerStatus {
+    case start
+    case pause
+    case end
+}
 
+class ViewController: UIViewController {
+    
+    //MARK: - Properties
+    @IBOutlet var timerLabel: UILabel!
+    @IBOutlet var progressView: UIProgressView!
+    @IBOutlet var datePicker: UIDatePicker!
+    @IBOutlet var cancelButton: UIButton!
+    @IBOutlet var toggleButton: UIButton!
+    
+    var duration = 60
+    var timerStatus: TimerStatus = .end
+    
+    //MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //configure
+        configureToggleButton()
     }
 
-
+    //MARK: - IBAction
+    //취소 버튼 이벤트함수
+    @IBAction func tapCancelButton(_ sender: UIButton) {
+        switch timerStatus {
+        case .start, .pause:
+            timerStatus = .end
+            cancelButton.isEnabled = false
+            setTimerInfoViewVisible(isHidden: true)
+            datePicker.isHidden = false
+            toggleButton.isSelected = false
+        default:
+            break
+        }
+    }
+    
+    //시작 버튼 이벤트함수
+    @IBAction func tapToggleButton(_ sender: UIButton) {
+        //데이트 픽커에서 설정한 시간을 초로 변환
+        duration = Int(datePicker.countDownDuration)
+        
+        switch timerStatus {
+        case .start:
+            timerStatus = .pause
+            toggleButton.isSelected = false
+        case .end:
+            timerStatus = .start
+            setTimerInfoViewVisible(isHidden: false)
+            datePicker.isHidden = true
+            toggleButton.isSelected = true
+            cancelButton.isEnabled = true
+        case .pause:
+            timerStatus = .start
+            toggleButton.isSelected = true
+        }
+    }
+    
+    //MARK: - Function
+    func setTimerInfoViewVisible(isHidden: Bool){
+        timerLabel.isHidden = isHidden
+        progressView.isHidden = isHidden
+    }
+    
+    func configureToggleButton(){
+        toggleButton.setTitle("시작", for: .normal)
+        toggleButton.setTitle("일시정지", for: .selected)
+    }
 }
 
